812ea866372fdac37e0b5a4bad231da147aeaa8e
karin0 <karin0@gmx.com>
2025-09-20 06:41:11 +0800

    discover hosts by zone subnets

--- cfg.py
+++ cfg.py
@@ -3,10 +3,12 @@ import datetime
 import importlib.util
 
 from configparser import ConfigParser
+from ipaddress import AddressValueError, IPv4Address, IPv4Network
 
 from moon.intf import Interfaces
 from moon.route import Zone, ZoneSet
 from moon.util import trace
+from moon.syn import Config
 
 if not os.environ.get('LUNA_STRICT_SUBNET'):
     try:
@@ -33,13 +35,13 @@ def check_timezone(hours: int) -> bool:
     return timezone == hours * 3600
 
 
-def in_zone(cfg: ConfigParser, zone: str) -> bool:
+def in_zone(cfg: ConfigParser, sect: str, zone: Zone) -> bool:
     # AND for timezone and subnet, so no constraint means always hits.
-    if tz := cfg.get(zone, 'timezone', fallback=None):
+    if tz := cfg.get(sect, 'timezone', fallback=None):
         if not check_timezone(float(tz)):
             return False
 
-    if subnets := cfg.get(zone, 'subnet', fallback='').split():
+    if subnets := zone.priv:
         global interfaces
         if not interfaces:
             trace('>Interfaces')
@@ -67,7 +69,7 @@ def load_hook(file):
 
 
 class ZoneConfig:
-    def __init__(self, file) -> None:
+    def __init__(self, file, conf: Config | None = None) -> None:
         self._cfg = cfg = ConfigParser()
         if not cfg.read(file):
             raise FileNotFoundError(file)
@@ -77,11 +79,77 @@ class ZoneConfig:
 
         self.zones: dict[str, Zone] = {}
         zones = self.zones
+        zone_stubs = []
+        vis = set()
+
+        if conf:
+            smart_stubs = []
 
         for sect in cfg.sections():
             hosts = cfg.get(sect, 'host', fallback='').split()
-            hosts = tuple(spec.split(':') for spec in hosts)
+            hosts = [spec.split(':') for spec in hosts]
+
+            for aliases in hosts:
+                for alias in aliases:
+                    if alias in vis:
+                        raise ValueError(f'Duplicate name {alias} in zone {sect}')
+                    vis.add(alias)
+
+            subnets = tuple(
+                IPv4Network(cidr, strict=False)
+                for cidr in cfg.get(sect, 'subnet', fallback='').split()
+            )
+
+            stub = (sect, hosts, subnets)
+            zone_stubs.append(stub)
+
+            if conf and not cfg.getboolean(sect, 'strict-host', fallback=False):
+                smart_stubs.append(stub)
+
+        if conf and smart_stubs:
+            # Find SSH hosts in the given subnets smartly.
+            all_hosts = curr_host = None
+            for host, hostname in sorted(conf.hostnames()):
+                if host in vis:
+                    continue
+
+                try:
+                    ip = IPv4Address(hostname)
+                except AddressValueError:
+                    continue
+
+                for zone, hosts, subnets in smart_stubs:
+                    for net in subnets:
+                        if ip in net:
+                            # Find a canonical host, now find all its aliases
+                            # with the same prefix.
+                            if all_hosts is None:
+                                all_hosts = iter(
+                                    h
+                                    for h in sorted(h for h in conf.hosts())
+                                    if h not in vis
+                                )
+
+                            aliases = [host]
+
+                            while curr_host != host:
+                                curr_host = next(all_hosts, None)
+
+                            while (
+                                curr_host := next(all_hosts, None)
+                            ) and curr_host.startswith(host):
+                                vis.add(curr_host)
+                                aliases.append(curr_host)
+
+                            hosts.append(aliases)
+                            break
+                    else:
+                        continue
+                    break
+
+        for sect, hosts, subnets in zone_stubs:
             zones[sect] = zone = g.add(sect, hosts)
+            zone.priv = subnets
 
             if hook := cfg.get(sect, 'hook', fallback=None):
                 hooks.append(load_hook(hook))
@@ -97,14 +165,14 @@ class ZoneConfig:
                 try:
                     via, to = parts
                     try:
-                        # via/to:cost
+                        # via|to:cost
                         cost = int(to)
                         spec = via
                     except ValueError:
                         # via:to
                         return zones[to], via, None
                 except ValueError:
-                    # via/to
+                    # via|to
                     spec = arc
                     cost = None
 
@@ -131,7 +199,7 @@ class ZoneConfig:
     def route(self):
         g = self._g
         for sect, zone in self.zones.items():
-            if in_zone(self._cfg, sect):
+            if in_zone(self._cfg, sect, zone):
                 g.set_src(zone)
 
         g.route()
